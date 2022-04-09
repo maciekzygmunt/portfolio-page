3328656b0b74c402c21c424f676ac3bee6c09f61
Maciej Zygmunt <maciekzygmunt@interia.pl>
2022-04-09 19:04:23 +0200

    minor performance imporvements

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,4 @@
-import Footer from './Footer';
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   Animator,
   ScrollContainer,
@@ -13,10 +12,11 @@ import {
   Sticky,
   StickyIn,
   ZoomIn,
-  FadeOut,
 } from 'react-scroll-motion';
 import { projects } from '../projects';
-import ProjectItem from './ProjectItem';
+
+const ProjectItem = React.lazy(() => import('./ProjectItem.js'));
+const Footer = React.lazy(() => import('./Footer.js'));
 
 function Feed() {
   const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
@@ -24,71 +24,73 @@ function Feed() {
 
   return (
     <>
-      <ScrollContainer>
-        <ScrollPage page={0}>
-          <div id="home">
-            <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
-              <span style={{ fontSize: '40px' }}>👋Hi, I'm Maciej Zygmunt</span>
-            </Animator>
-          </div>
-        </ScrollPage>
-        <ScrollPage page={1}>
-          <Animator animation={ZoomInScrollOut}>
-            <span style={{ fontSize: '40px' }}>👨‍💻Web Front-end Developer</span>
-          </Animator>
-        </ScrollPage>
-        <ScrollPage page={2}>
-          <div id="skills">
-            <Animator animation={FadeUp}>
-              <span style={{ fontSize: '40px' }}>🤹Skills:</span>
-            </Animator>
-          </div>
-        </ScrollPage>
-        <ScrollPage page={3}>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              height: '100%',
-            }}
-          >
-            <span
-              style={{
-                fontSize: '40px',
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-              }}
-            >
-              <Animator animation={MoveIn(-1000, 0)}>HTML5</Animator>
-              <Animator animation={MoveIn(1000, 0)}>CSS3</Animator>
-              <Animator animation={MoveIn(-1000, 0)}>React.js</Animator>
-              <Animator animation={MoveIn(1000, 0)}>Redux</Animator>
-              <Animator animation={MoveIn(-1000, 0)}>React Router</Animator>
-              <Animator animation={MoveIn(1000, 0)}>Styled Components</Animator>
-              <Animator animation={MoveIn(-1000, 0)}>Git</Animator>
-            </span>
-          </div>
-        </ScrollPage>
-        <ScrollPage page={4}>
-          <div id="projects">
-            <Animator animation={FadeUp}>
-              <span style={{ fontSize: '40px', whiteSpace: 'pre' }}>🛠️Projects:</span>
-            </Animator>
-          </div>
-        </ScrollPage>
-        {projects.map((project, i) => (
-          <ScrollPage page={i + 5}>
-            <Animator animation={MoveIn(-1000, 0)}>
-              <ProjectItem key={project.id} project={project} />
+      <Suspense>
+        <ScrollContainer>
+          <ScrollPage page={0}>
+            <div id="home">
+              <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+                <span style={{ fontSize: '40px' }}>👋Hi, I'm Maciej Zygmunt</span>
+              </Animator>
+            </div>
+          </ScrollPage>
+          <ScrollPage page={1}>
+            <Animator animation={ZoomInScrollOut}>
+              <span style={{ fontSize: '40px' }}>👨‍💻Web Front-end Developer</span>
             </Animator>
           </ScrollPage>
-        ))}
-      </ScrollContainer>
-      <div id="contactme">
-        <Footer />
-      </div>
+          <ScrollPage page={2}>
+            <div id="skills">
+              <Animator animation={FadeUp}>
+                <span style={{ fontSize: '40px' }}>🤹Skills:</span>
+              </Animator>
+            </div>
+          </ScrollPage>
+          <ScrollPage page={3}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+              }}
+            >
+              <span
+                style={{
+                  fontSize: '40px',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                }}
+              >
+                <Animator animation={MoveIn(-1000, 0)}>HTML5</Animator>
+                <Animator animation={MoveIn(1000, 0)}>CSS3</Animator>
+                <Animator animation={MoveIn(-1000, 0)}>React.js</Animator>
+                <Animator animation={MoveIn(1000, 0)}>Redux</Animator>
+                <Animator animation={MoveIn(-1000, 0)}>React Router</Animator>
+                <Animator animation={MoveIn(1000, 0)}>Styled Components</Animator>
+                <Animator animation={MoveIn(-1000, 0)}>Git</Animator>
+              </span>
+            </div>
+          </ScrollPage>
+          <ScrollPage page={4}>
+            <div id="projects">
+              <Animator animation={FadeUp}>
+                <span style={{ fontSize: '40px', whiteSpace: 'pre' }}>🛠️Projects:</span>
+              </Animator>
+            </div>
+          </ScrollPage>
+          {projects.map((project, i) => (
+            <ScrollPage key={project.id} page={i + 5}>
+              <Animator animation={MoveIn(-1000, 0)}>
+                <ProjectItem key={project.id} project={project} />
+              </Animator>
+            </ScrollPage>
+          ))}
+        </ScrollContainer>
+        <div id="contactme">
+          <Footer />
+        </div>
+      </Suspense>
     </>
   );
 }
--- src/projects.js
+++ src/projects.js
@@ -9,14 +9,14 @@ export const projects = [
   {
     id: 2,
     name: 'Todo app',
-    photo: 'https://i.ibb.co/Wk893ZS/image2.jpg',
+    photo: 'https://i.ibb.co/PzJHt41/image2.jpg',
     repo: 'https://github.com/maciekzygmunt/todo-app',
     demo: 'https://todo-app-ten-alpha.vercel.app',
   },
   {
     id: 3,
     name: 'Instagram clone',
-    photo: 'https://i.ibb.co/Yk8hQT5/image1.jpg',
+    photo: 'https://i.ibb.co/K57Qvqz/image1.jpg',
     repo: 'https://github.com/maciekzygmunt/instagram-clone',
     demo: 'https://instagram-clone-ten-tau.vercel.app',
   },
